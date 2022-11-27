ff14ab42324aa2915dbd7c3c8ca1e0a61b869ab9
PP-2022 <2733286080@qq.com>
2022-11-27 20:49:49 +0800

    This reverts commit 7c52eeb7

--- recruit/src/main/java/com/pzz/controller/UserController.java
+++ recruit/src/main/java/com/pzz/controller/UserController.java
@@ -3,7 +3,17 @@ package com.pzz.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 彭政
+ * @since 2022-11-27
+ */
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UserController {
+
 }
+
